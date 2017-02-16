c432c1716d87d2d7d49f38427d571ac088232129
juliepesnel <juliepesnel@gmail.com>
2017-02-16 16:07:52 +0100

    cocktails nearly done test

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,6 +1,6 @@
 class Cocktail < ApplicationRecord
   has_many :doses, dependent: :destroy
-  has_many :ingredient, through: :doses
+  has_many :ingredients, through: :doses
   validates :name, presence:true, uniqueness:true, allow:nil
 
 end
--- app/models/dose.rb
+++ app/models/dose.rb
@@ -1,5 +1,5 @@
 class Dose < ApplicationRecord
- belongs_to :ingredients
+ belongs_to :ingredient
  belongs_to :cocktail
  validates :description, presence:true, uniqueness:true, allow:nil
  validates :ingredient, presence:true, uniqueness:true, allow:nil
