4a96d502cc8720a503f3213246b8d1a2da715bda
juliepesnel <juliepesnel@gmail.com>
2017-02-17 12:36:39 +0100

    back end done

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -1,8 +1,6 @@
 class Dose < ApplicationRecord
- belongs_to :ingredient
- belongs_to :cocktail
- validates :description, presence:true, uniqueness:true, allow:nil
- validates :ingredient, presence:true, uniqueness:true, allow:nil
- validates :cocktail, presence:true, uniqueness:true, allow:nil
-
+  belongs_to :ingredient
+  belongs_to :cocktail
+  validates :description, presence: true
+  validates_uniqueness_of :cocktail, scope: :ingredient
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
 
   root to:'cocktails#index'
 
-  resources :cocktails
+  resources :cocktails do
+
+    resources :doses, only: [:create]
+  end
+
+  resources :doses, only: [:destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
